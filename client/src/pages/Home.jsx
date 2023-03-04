53b3905e2796d69b9faa8d7fa75ecadd17eca118
Muhammad <120216920+komilovmDev@users.noreply.github.com>
2023-03-04 15:54:57 +0500

    home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -23,6 +23,9 @@ import soft2 from '../images/soft2.svg'
 import soft3 from '../images/soft3.svg'
 import soft4 from '../images/soft4.svg'
 import datalogo from '../images/datalogo.png'
+import gps from '../images/gps.svg'
+import phone from '../images/phone.svg'
+import sms from '../images/sms.svg'
 
 
 
@@ -187,7 +190,38 @@ export default function Home() {
             <footer>
                 <div className="footerCon">
                     <div className="footerTop">
-                        
+                        <div className="footerContact">
+                            <a href="" className="footeradress">
+                                <button><img src={phone} alt="" /></button>
+                                <span>+1 (646) 418 9064</span>
+                            </a>
+                            <a href="" className="footeradress">
+                                <button><img src={sms} alt="" /></button>
+                                <span>info@axelhub.com</span>
+                            </a>
+                            <a href="" className="footeradress">
+                                <button><img src={gps} alt="" /></button>
+                                <span>New York (NY), Tampa (FL), Bishkek (KG)</span>
+                            </a>
+                        </div>
+                        <div className="footerInfos">
+                            <ul>
+                                <span>Service</span>
+                                <li>UI/UX Design</li>
+                                <li>Mobile Development</li>
+                                <li>Web Development</li>
+                            </ul>
+                            <ul>
+                                <span>Company</span>
+                                <li>About Us</li>
+                                <li>Career Opportunities</li>
+                                <li>FAQ's</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="footerBottom">  
+                        <span>2023 AxelHub, All rights reserved</span>
+                        <img src={datalogo} alt="" />
                     </div>
                 </div>
             </footer>
